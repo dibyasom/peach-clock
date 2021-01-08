290e45411b28185915b93b0088038a05171f67b6
vhaegar1526 <dibya.secret@gmail.com>
2021-01-08 15:53:49 +0530

    Scratch.js

---
+++ Scripts/scratch1.js
@@ -0,0 +1,13 @@
+const pocket_money= 500;
+const expenditure= 300;
+const annual_income= (pocket_money-expenditure)*12;
+console.log(annual_income);
+
+const myName= "Dibyasom Puhan";
+// Put strings b/w `` if you wanna add variables in b/w.
+const greet= `Hello, ${myName}!! how're you?`;
+console.log(greet);
+
+const brainFucked= true;
+console.log(22/7);
+
